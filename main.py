08a3ea3fb9e28f27af8c107257f4f0421445bfe8
Dwojak Artur <dwojaka207@gmail.com>
2022-12-08 11:04:34 +0100

    Initial version

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# from moviepy.editor import VideoFileClip
-#
-# video = VideoFileClip("naruto1.mp4")
-# video.audio.write_audiofile("naruto_odcinek_sound2.mp3", bitrate="300k")
+from moviepy.editor import VideoFileClip
+import numpy
 import librosa
 from scipy import signal
-import numpy as np
 
-y_within, sr_within = librosa.load("naruto1.mp3", sr=None)
-y_find, _ = librosa.load("opening.mp3", sr=sr_within)
 
-c = signal.correlate(y_within, y_find, mode='valid', method='fft')
+class VideoRemover:
+    VIDEO_BITRATE = "300k"
 
-peak = np.argmax(c)
-offset = round(peak / sr_within, 2)
-print(offset)
+    def __init__(self, video_file_path, opening_path, ending_path):
+        self.video_file_path = video_file_path
+        self.parsed_video_file_path = f"{video_file_path}_tmp.mp3"
+        self.opening_path = opening_path
+        self.ending_path = ending_path
+
+    def mp4_to_mp3(self):
+        video = VideoFileClip(self.video_file_path)
+        video.audio.write_audiofile(self.parsed_video_file_path, bitrate=self.VIDEO_BITRATE)
+
+    def librosa_load(self, filename, sampling_rate=None):
+        return librosa.load(filename, sr=sampling_rate)
+
+    def calculate_offset(self, filename, y_movie, sampling_rate_movie):
+        y_sample, _ = self.librosa_load(filename, sampling_rate_movie)
+        correlation = signal.correlate(y_movie, y_sample, mode='valid', method='fft')
+        peak = numpy.argmax(correlation)
+        return round(peak / sampling_rate_movie, 2)
+
+    def parse_video_file(self):
+        opening_offset, ending_offset = None, None
+        # self.mp4_to_mp3()
+        y_movie, sampling_rate_movie = self.librosa_load(self.parsed_video_file_path)
+        if self.opening_path:
+            opening_offset = self.calculate_offset(self.opening_path, y_movie, sampling_rate_movie)
+
+        print(opening_offset, ending_offset)
+
+
+aa = VideoRemover("naruto1.mp4", "opening.mp3", "")
+aa.parse_video_file()
