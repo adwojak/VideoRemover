9e387a10dfa0d8cc5cd521974dd69f096424d296
Dwojak Artur <dwojaka207@gmail.com>
2022-12-16 17:30:00 +0100

    Rendering movie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from moviepy.audio.io.AudioFileClip import AudioFileClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 import moviepy
 import numpy
 import librosa
@@ -27,7 +27,7 @@ class VideoRemover:
         correlation = signal.correlate(y_movie, audio_sample, mode='valid', method='fft')
         audio_starting_time = numpy.argmax(correlation) / sampling_rate_movie
         duration = librosa.get_duration(audio_sample, audio_sampling_rage)
-        return round(audio_starting_time, 2), round(audio_starting_time + duration, 2)
+        return float(round(audio_starting_time, 2)), float(round(audio_starting_time + duration, 2))
 
     def calculate_offsets(self):
         opening_offset, ending_offset = None, None
@@ -35,16 +35,33 @@ class VideoRemover:
 
         if self.opening_path:
             opening_offset = self.calculate_audio_offset(self.opening_path, y_movie, sampling_rate_movie)
-        # if self.ending_path:
-        #     ending_offset = self.calculate_single_offset(self.ending_path, y_movie, sampling_rate_movie)
+        if self.ending_path:
+            ending_offset = self.calculate_audio_offset(self.ending_path, y_movie, sampling_rate_movie)
         return opening_offset, ending_offset
 
+    def calculate_subtimes(self, opening_offset, ending_offset):
+        subtimes = []
+        if opening_offset:
+            subtimes.extend(((0, opening_offset[0]), opening_offset[1]))
+        if ending_offset:
+            subtimes.extend(((subtimes.pop(), ending_offset[0]), ending_offset[1]))
+        return subtimes
+
     def write_video_file(self, opening_offset, ending_offset):
-        # Subclipping and merging files
+        subtimes = self.calculate_subtimes(opening_offset, ending_offset)
         with VideoFileClip(self.video_file_path) as video:
-            video = video.cutout(opening_offset[0], opening_offset[1])
+            subclips = []
+            for subtime in subtimes:
+                if isinstance(subtime, tuple):
+                    subclips.append(video.subclip(*subtime))
+                else:
+                    subclips.append(video.subclip(subtime))
+            # video = video.subclip(ending_offset[0], ending_offset[1])
+            # video = video.subclip(opening_offset[0], opening_offset[1])
+            # video = video.cutout(opening_offset[0], opening_offset[1])
             # TODO Error with get_frame for cutted video
-            video.write_videofile(f"NEW_{self.video_file_path}", codec='libx264')
+            final_video = concatenate_videoclips(subclips)
+            final_video.write_videofile(f"NEW_{self.video_file_path}")
 
     def create_new_video_file(self):
         # self.extract_audio_file()
@@ -59,3 +76,4 @@ ending_file = "ending.mp3"
 
 video_remover = VideoRemover(parsed_file, opening_file, ending_file)
 video_remover.create_new_video_file()
+# video_remover.write_video_file(1, 1)
